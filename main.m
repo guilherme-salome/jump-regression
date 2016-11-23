fd0d4132507eb87acefbf45396a7f3519f19984e
Guilherme Salome <guilhermesalome@gmail.com>
2016-11-22 20:57:24 -0500

    Added comments to main and separateReturns. Added plot of price.

--- main.m
+++ main.m
@@ -11,17 +11,19 @@ kn = 11;
 
 %% Load Data for SPY
 filename = 'data/SPY_5min.dat';
+tkr = 'SPY';
 raw = load(filename);
 
 %% SPY: Extract Returns, BV, TOD, Jump CUT, Diffusive Returns and Jump Returns
 [ret,dates] = getReturnAndDate(raw(:,1:2),raw(:,3),n,T);
-BV = getBV(ret,n,T);
-tod = getTOD(ret,n,T);
-cut = getCUT(alpha,tod,BV,delta_n);
-[r_c,r_d] = separateReturns(ret,cut);
+BV = getBV(ret,n,T); % bipower variance
+tod = getTOD(ret,n,T); % time of day factor
+cut = getCUT(alpha,tod,BV,delta_n); % jump threshold
+[r_c,r_d] = separateReturns(ret,cut); % diffusive and jump returns
 
 %% Load Stock Data
 stocks = {'AIG_5min.dat','BLK_5min.dat','CB_5min.dat','C_5min.dat','GNTX_5min.dat','MET_5min.dat','MMC_5min.dat','MS_5min.dat','PNC_5min.dat','STT_5min.dat','TRV_5min.dat'}';
+stkr = 'AIG';
 sraw = load('data/AIG_5min.dat');
 
 %% STOCK: Extract Returns, BV, TOD, Jump CUT, Diffusive Returns and Jump Returns
@@ -40,3 +42,16 @@ nj = length(jump_loc); % number of jumps
 [sigma,R] = getSpotVol(c,Q,nj);
 [beta,beta_tilde] = jumpReg(sret,ret,Q,c,jump_loc,nj);
 [CI_low, CI_up] = jumpRegCI(beta,0.05,ret,c,Q,jump_loc,nj,delta_n);
+
+%% Plot everything
+
+% plot stock price against market
+f1 = figure;
+plot(getSerialDate(sraw(:,1:2)),sraw(:,3),'LineWidth',0.5,'Color','blue');
+hold on;
+plot(getSerialDate(raw(:,1:2)),raw(:,3),'LineWidth',0.5,'Color','red'); 
+datetick('x','yyyy');
+xlabel('Date: t','Interpreter','latex');
+ylabel('Price: $P_t$','Interpreter','latex');
+title(['Price of ' stkr ' and ' tkr],'Interpreter','latex');
+legend({['Price of ' stkr],['Price of ' tkr]},'Interpreter','latex');
--- scripts/separateReturns.m
+++ scripts/separateReturns.m
@@ -1,4 +1,16 @@
 function [r_c,r_d] = separateReturns(ret,cut)
+% separateReturns separates diffusive and jump returns
+%
+% INPUT:
+% ret: n*Tx1 vector of returns
+% cut: n*Tx1 vector of jump thresholds
+%
+% OUTPUT:
+% r_c: n*Tx1 vector of diffusive returns, with zeroes where the returns are
+%      jump returns
+% r_d: n*Tx1 vector of jump returns, with zeroes where the returns are
+%      diffusive returns
+%
 
 r_c = ret;
 r_c(abs(r_c) > cut) = 0; % if return is jump, set it to zero
