c06dfcec0dabae48371e45819dbe10932eb14402
Guilherme Salome <guilhermesalome@gmail.com>
2016-11-23 14:41:49 -0500

    Made it clear that the null hypothesis is tested at 5%

--- scripts/plotJumpReg.m
+++ scripts/plotJumpReg.m
@@ -9,7 +9,7 @@ x_grid = 1.1*min(ret(jump_loc)):0.01:1.1*max(ret(jump_loc));
 hold on; grid on;
 plot(x_grid,beta.*x_grid,'LineWidth',1,'Color','blue');
 plot(NaN,NaN,'-s','MarkerFaceColor','yellow');
-f2l = legend({[tkr ' return vs. ' stkr ' jump return'],['Jump Regression: $\hat{r}_t^{' stkr '}=\hat{\beta}_{jump}r_t^{d,' tkr '}$'],['$\hat{\beta}_{jump}=' num2str(beta) '$, CI:[' num2str(CI_low) ',' num2str(CI_up) '], $H_0$: ' rej]},'Interpreter','latex','Location','northwest');
+f2l = legend({[tkr ' return vs. ' stkr ' jump return'],['Jump Regression: $\hat{r}_t^{' stkr '}=\hat{\beta}_{jump}r_t^{d,' tkr '}$'],['$\hat{\beta}_{jump}=' num2str(beta) '$, CI:[' num2str(CI_low) ',' num2str(CI_up) '], $H_0^{5\%}$: ' rej]},'Interpreter','latex','Location','northwest');
 % change font size
 set(findall(f2,'type','text'),'FontSize',15);
 set(f2l,'FontSize',11);
